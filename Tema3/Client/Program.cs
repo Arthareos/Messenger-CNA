3eb76cc8a3da0069ab5c23d57a2afdda549b069d
AlexandraS <alexandrastan1171@gmail.com>
2020-05-17 16:37:14 +0300

    Client + Server basic

---
+++ Tema3/Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+using Tema3;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool isValid = false;
+            string name;
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            name = Console.ReadLine();
+
+
+            var clientBirthDate = new HelloRequest { Name = name };
+            var reply = await client.SayHelloAsync(clientBirthDate);
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+        }
+    }
+}
