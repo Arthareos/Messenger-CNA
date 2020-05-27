f8f2e36c4af65a688a0b9f7b5cf89d94bac1ec84
AlexandraS <alexandrastan1171@gmail.com>
2020-05-28 00:36:06 +0300

    ChatRooms +ChatRoomService
    Am adaugat o serie de task-uri care adauga un client la un chatroom, care afiseaza mesajele din chatroom,  care conecteaza clientul la chat, si care deconecteaza clientul de la chat.
    Am adaugat clasa model ChatRoom.

---
+++ Tema3/Tema3/Models/ChatRoom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class ChatRoom
+    {
+        private int id;
+        private List<Client> clientsInRoom;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+        }
+        public List<Client> ClientsInRoom
+        {
+            get
+            {
+                return clientsInRoom;
+            }
+        }
+        public ChatRoom(int id)
+        {
+            this.id = id;
+            clientsInRoom = new List<Client>();
+        }
+    }
+}
---
+++ Tema3/Tema3/Services/ChatRoomService.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Core;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services
+{
+    public class ChatRoomService
+    {
+		private readonly ChatRoom chatRoom;
+
+		public ChatRoomService(ChatRoom chatRoom)
+		{
+			this.chatRoom = chatRoom;
+		}
+
+		public async Task BroadcastMessageAsync(ChatMessage message)
+		{
+			foreach (var client in this.chatRoom.ClientsInRoom)
+			{
+				await client.Stream.WriteAsync(message);
+				Console.WriteLine($"Sent message from {message.ClientName} to {client.Name}");
+			}
+		}
+
+		public Task<int> AddClientToChatRoomAsync(ClientDetails customer)
+		{
+			chatRoom.ClientsInRoom.Add(new Client
+			{
+				Color = customer.ColorInConsole,
+				Name = customer.Name,
+				ClientId = int.Parse(customer.Id)
+			});
+			return Task.FromResult(chatRoom.Id);
+		}
+
+		public void ConnectCustomerToChatRoom(int roomId, int customerId, IAsyncStreamWriter<ChatMessage> responseStream)
+		{
+			chatRoom.ClientsInRoom.FirstOrDefault(c => c.ClientId == customerId).Stream = responseStream;
+		}
+
+		public void DisconnectCustomer(int roomId, int customerId)
+		{
+			chatRoom.ClientsInRoom.Remove(chatRoom.ClientsInRoom.FirstOrDefault(c => c.ClientId == customerId));
+		}
+	}
+}
