9f0c2e0323b75da6a37b25394d997976b21e3648
VGog99 <vladgogu14@gmail.com>
2020-06-01 17:34:41 +0300

    InceputGrafica
    Proiect nou care contine o idee pentru interfata grafica a chat-ului

---
+++ MessengerUIDesign/MessengerUIDesign/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MessengerUIDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void mnuShowParticipants_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new ParticipantsView();
+            window.Show();
+            Close();
+            return;
+        }
+        private void mnu_QuitChat_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Aici o sa scriem si trimitem qt! ca sa oprim stream-ul");
+            Close();
+        }
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtChat.Text))
+                txtChat.Text = txtMessage.Text;
+            else
+                txtChat.Text = txtChat.Text + "\n" + txtMessage.Text;
+            txtMessage.Clear();
+        }
+
+    }
+}
---
+++ MessengerUIDesign/MessengerUIDesign/ParticipantsView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MessengerUIDesign
+{
+    /// <summary>
+    /// Interaction logic for ParticipantsView.xaml
+    /// </summary>
+    public partial class ParticipantsView : Window
+    {
+        public ParticipantsView()
+        {
+            InitializeComponent();
+        }
+        private void mnuHideParticipants1_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new MainWindow();
+            window.Show();
+            Close();
+            return;
+        }
+
+        private void mnuQuitChat_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Aici o sa scriem si trimitem qt! ca sa oprim stream-ul");
+            Close();
+        }
+    }
+}
